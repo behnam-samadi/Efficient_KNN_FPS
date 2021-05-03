e42a27aff5f05c3189085b2c785e39188f633b4b
behnam samadi <samadi.behnam70@gmail.com>
2021-05-03 17:00:51 +0430

    multi thread but error!

--- knn.cpp
+++ knn.cpp
@@ -59,8 +59,8 @@ class Frame{
 
 struct thread_data
 {
-    Frame query;
-    Frame reference;
+    Frame *query;
+    Frame *reference;
     int K;
     int num_ref;
     vector<vector<int>> result;
@@ -95,6 +95,7 @@ return(frame);
 }
 
 vector<vector<int>> KNN (Frame reference, Frame query, int K, int num_query=0){
+    cout<<"one thread is working";
     int num_ref_points = reference.data.size();
     int num_query_points = query.data.size();
     if (!(num_query == 0)) num_query_points = num_query;
@@ -111,14 +112,53 @@ vector<vector<int>> KNN (Frame reference, Frame query, int K, int num_query=0){
         {
             result[i][c] = topk[c];
         }
+
     }
+    cout<<"one thread is reurnin";
 return(result);
 }
 
 void * call_thread (void* args){
+    
     thread_data arguments = *((thread_data*) args);
-    vector<vector<int>> knn = KNN(arguments.reference, arguments.query, arguments.K, arguments.num_ref);
+    
+    Frame * a = arguments.reference;
+    Frame * b = arguments.query;
+    int c = arguments.K;
+    int d = arguments.num_ref;
+    cout<<"one thread is running";
+    vector<vector<int>> knn = KNN(*(arguments.reference), *(arguments.query), arguments.K, arguments.num_ref);
+    cout<<endl<<"one thread is writing result";
     ((thread_data*)args)->result = knn;
+    cout<<endl<<"-----------------one thred wrote the result";
+    return (0);
+
+}
+
+void print_frame (Frame frame, int num_points){
+    cout<<"it has size:" << frame.data.size()<<" "<<frame.data[0].size()<<endl;
+    for (int i = 0; i< num_points;i++)
+    {
+        for(int j = 0; j<frame.data[0].size();j++)
+        {
+            cout<<frame.data[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+void print_vector_2D (vector<vector<float>>input, int num_points){
+    for (int i = 0; i< num_points;i++)
+    {
+        for(int j = 0; j<input[0].size();j++)
+        {
+            cout<<input[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 }
 
 
@@ -141,13 +181,16 @@ int main(){
     }
     std::vector<float> v1 = {1,2,3};
     std::vector<float> v2 = {4,6,3};
-    cout<<"\n"<<"distance: "<<calc_distance(v1,v2, "Manhattan");
+    //cout<<"\n"<<"distance: "<<calc_distance(v1,v2, "Manhattan");
     const clock_t begin = clock();
     int number_of_ref_points = 64;
-    vector<vector<int>> knn = KNN(frame1, frame2, 20, number_of_ref_points);
+    int number_of_nearest_point = 20;
+    vector<vector<int>> knn = KNN(frame1, frame2, number_of_nearest_point, number_of_ref_points);
     float run_time = float(clock() - begin);
     cout<<endl<< "run_time" <<run_time;
     int num_threads = 4;
+    //print_frame(frame2, 12);
+    
 
     //slicing the data;
     Frame slices[num_threads];
@@ -165,11 +208,56 @@ int main(){
         slices[i].num_points = slice_size;
         slices[i].points_dim = frame_channels;
     }
-cout<<endl<<endl<<"az inja";
-    int FN = 1;
-    for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<slices[FN].data[j][c]<<" ,";
-        cout<<endl<<"az inja2:";
-    for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<frame2.data[FN*slice_size+ j][c]<<" ,";
+    print_frame(slices[2], slices[2].data.size());
+
+
+    pthread_t threads[num_threads];
+    thread_data data_for_threads[num_threads];
+    cout<<"preparing data"<<endl;
+    for (int t= 0; t < num_threads;t++)
+    {
+        data_for_threads[t].reference = &frame1;
+        data_for_threads[t].query = &(slices[t]);
+        //imidiate
+        data_for_threads[t].K = number_of_nearest_point;
+        data_for_threads[t].num_ref = number_of_ref_points;
+    }
+    cout<<"------"<<endl;
+    print_frame(*(data_for_threads[2].query), 16);
+    cout<<"and---"<<endl;
+    print_vector_2D(slices[2].data, 16);
+    cout<<"----------";
+    exit(0);
+    cout<<"creating threads"<<endl;
+
+    for(int t = 0 ; t<num_threads;t++)
+    {
+        pthread_create(&(threads[t]), NULL, call_thread, (void*)(&(data_for_threads[t])));
+    }
+    
+
+
+    cout<<"joining threads"<<endl;
+
+    for(int t = 0; t<num_threads;t++)
+    {
+        pthread_join(threads[t], NULL);
+    }
+    cout<<"threads joined";
+
+    for(int  t = 0; t<num_threads;t++)
+    {
+        cout<<data_for_threads[t].result[0][0];
+    }
+    cout<<"asle dastan:"<<endl;
+    //print_vector_2D(data_for_threads[0].result, 12);
+    cout<<endl;
+cout<<"finishied";
+//cout<<endl<<endl<<"az inja";
+    //int FN = 1;
+    //for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<slices[FN].data[j][c]<<" ,";
+        //cout<<endl<<"az inja2:";
+    //for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<frame2.data[FN*slice_size+ j][c]<<" ,";
     //cout<<endl<<slices[2].data;
 
     //cout<<knn.size()<<"\n";
---
+++ multi_thread_example.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+using namespace std;
+
+void *print_message_function(void *a);
+
+struct arg_to_thread{
+     int size;
+     int start_value;
+     char message;
+
+};
+
+int main()
+{
+     pthread_t thread1, thread2;
+     arg_to_thread arg1;
+     arg_to_thread arg2;
+     arg1.size = 100000;
+     arg2.size = 100000;
+     arg1.start_value = -100000;
+     arg2.start_value = 50000;
+     arg1.message = 'F';
+     arg2.message = 'S';
+
+
+     int message1[10000];
+     int message2[10000];
+     int  iret1, iret2;
+
+    /* Create independent threads each of which will execute function */
+
+     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*)&arg1);
+     cout<<"p";
+     iret2 = pthread_create( &thread2, NULL, print_message_function, (void*)&arg2);
+
+     /* Wait till threads are complete before main continues. Unless we  */
+     /* wait we run the risk of executing an exit which will terminate   */
+     /* the process and all threads before the threads have completed.   */
+
+     pthread_join( thread1, NULL);
+     pthread_join( thread2, NULL); 
+
+     printf("Thread 1 returns: %d\n",iret1);
+     printf("Thread 2 returns: %d\n",iret2);
+     exit(0);
+}
+
+void* print_message_function(void* a )
+{
+     arg_to_thread args = *((arg_to_thread*)a);
+     for (int i= args.start_value;i<args.start_value + args.size;i++){
+          cout<<args.message<<" ";
+
+     }
+
+
+}
