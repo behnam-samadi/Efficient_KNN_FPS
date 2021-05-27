62a20054005718dee229c81002e49c6db19cbf18
behnam samadi <samadi.behnam70@gmail.com>
2021-05-27 14:22:09 +0430

    before starting main implementation

--- knn.cpp
+++ knn.cpp
@@ -10,31 +10,21 @@ using namespace std;
 
 
 
-vector<int> topK(vector<float> input, int K){
-    float inf = 0;
-    for (int i = i ; i< input.size();i++)
+void print_vector (vector<float> v){
+    for (int i = 0 ; i< v.size();i++)
     {
-        if (input[i] > inf) inf = i;
+        cout<<endl<<i<<" ";
     }
-    inf = inf + 100;
-    vector<int> result(K);
-    for (int c = 0; c<K; c++){
-        int min_arg = 0;
-        for (int j = 0; j<input.size();j++)
-        {
-            if(input[j] < input[min_arg]){
-                min_arg = j;
-            }
-        }
-        result[c]  = min_arg;
-        input[min_arg] = inf;
-    }    
-return (result);
+    cout<<endl;
 }
 
 
 float calc_distance (vector<float> v1, vector<float> v2, string type)
 {
+    cout<<"calc_distance" << v1.size() <<v2.size()<<endl;
+    print_vector(v1);
+    print_vector(v2);
+
     float sum = 0;
     for(int i = 0; i<v1.size();i++)
     {
@@ -85,34 +75,24 @@ struct thread_data
     Frame *reference;
     int start_reference;
     int end_reference;
-    vector<float> result;
+    vector<float> * result;
 };
 
 
 
-void print_vector_2D_int (vector<vector<int>>input){
-    cout<<"prinintg" <<"size"<<input.size()<<" "<<input[0].size();
-    for (int i = 0; i< input.size();i++)
-    {
-        for(int j = 0; j<input[0].size();j++)
-        {
-            cout<<input[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-
-return;}
 
 
 void * call_thread (void* args)
 {
+
     thread_data * args_c = (thread_data*)args;
+    
     for (int i = args_c->start_reference; i<args_c->end_reference;i++ )
     {
-        
          float res = calc_distance(args_c->query_point, args_c->reference->data[i],"Manhattan");
-         (args_c->result)[i] = res;
+         (*(args_c->result))[i] = res;
     }
+    cout<<"the thread with data" << args_c->start_reference <<" "<<args_c->end_reference<<"wrote the result"<<endl;
 }
 
 
@@ -143,17 +123,17 @@ void print_vector_2D (vector<vector<float>>input){
 
 }
 
-vector<float> calc_distance_multi_thread (vector<float> query_point, frame *reference)
+vector<float> calc_distance_multi_thread (vector<float> query_point, Frame *reference)
 {
-    int num_threads = 4;
+    int num_threads = 12;
     pthread_t threads[num_threads];
     thread_data data_for_threads[num_threads];
     int slice_size = floor(reference->data.size()/num_threads) ;
-    vector<vector<int>> result;
+    vector<float> result;
     for (int t = 0 ; t<num_threads;t++)
     {
     data_for_threads[t].query_point = query_point;
-    data_for_threads.reference = reference;
+    data_for_threads[t].reference = reference;
     data_for_threads[t].start_reference = t * slice_size;
     data_for_threads[t].end_reference = (t+1) * slice_size;
     data_for_threads[t].result = &result;
@@ -175,7 +155,7 @@ vector<float> calc_distance_multi_thread (vector<float> query_point, frame *refe
     }
     cout<<"threads joined";
 
-    return(data_for_threads[0].result);
+    return(*(data_for_threads[0].result));
 
 
 }
@@ -191,84 +171,13 @@ int main(){
 
     for(int i = 0; i<num_query_points;i++)
     {
-        print_vector_2D(calc_distance_multi_thread(query.data[i],&reference.data));
+        vector<float> result = calc_distance_multi_thread(query.data[i],&reference);
+        for (int c =0; c<result.size();c++)
+        {
+            cout<<endl<<result[c]<<" ";
+        }
+        cout<<endl;
     }
-
-
-    exit(0);
-
-
-    //for(int i = 0 ; i<)
-
-    int number_of_ref_points = 256;
-    int number_of_nearest_point = 20;
-    int num_threads = 4;
-
-
-    int slice_size = number_of_ref_points / num_threads;
-    pthread_t threads[num_threads];
-    thread_data data_for_threads[num_threads];
-    cout<<"preparing data"<<endl;
-    for (int t= 0; t < num_threads;t++)
-    {
-        data_for_threads[t].reference = &reference;
-        data_for_threads[t].query = &query;
-        data_for_threads[t].K = number_of_nearest_point;
-        data_for_threads[t].start_query = (t*slice_size);
-        data_for_threads[t].end_query = (t+1) * slice_size;
-        data_for_threads[t].order = t%2;
-        
-    }
-
-
-
-    for(int t = 0 ; t<num_threads;t++)
-    {
-        pthread_create(&(threads[t]), NULL, call_thread, (void*)(&(data_for_threads[t])));
-    }
-    
-
-
-    cout<<"joining threads"<<endl;
-
-    for(int t = 0; t<num_threads;t++)
-    {
-        pthread_join(threads[t], NULL);
-    }
-    cout<<"threads joined";
-
-    //for(int  t = 0; t<num_threads;t++)
-    //{
-      //  cout<<data_for_threads[t].result[0][0];
-    //}
-        float run_time = float(clock() - begin_time);
-    cout<<endl<< "run_time" <<run_time;
-    cout<<"asle dastan:"<<endl;
-    cout<<data_for_threads[0].result.size();
-    //print_vector_2D_int(data_for_threads[0].result);
-    cout<<endl;
-cout<<"finishied";
-//cout<<endl<<endl<<"az inja";
-    //int FN = 1;
-    //for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<slices[FN].data[j][c]<<" ,";
-        //cout<<endl<<"az inja2:";
-    //for (int j =0; j<slice_size;j++) for (int c = 0; c<frame_channels;c++)cout<<" "<<frame2.data[FN*slice_size+ j][c]<<" ,";
-    //cout<<endl<<slices[2].data;
-
-    //cout<<knn.size()<<"\n";
-    //cout<<knn[0].size()<<"\n";
-    //cout<<"inja:"<<"\n"<<"\n";
-    //cout<<endl;
-    //for (int i = 0; i<knn.size();i++){
-        //for (int j = 0; j< knn[0].size();j++){
-        //cout<< knn[i][j]<<"\n";}
-        //cout<<endl;
-
-    //}
-    
-    
-    //cout<<frame1.num_points<<" "<<frame1.points_dim<<"\n";
-    //cout<<"\n" << frame2.num_points<<" "<<frame2.points_dim;
     
 return (0);
 }
